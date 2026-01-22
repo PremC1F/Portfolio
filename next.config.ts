f5d1730d764d4338556ee2b27f0a48fe29981165
Prem Babu <kanaparthiprem03@gmail.com>
2026-01-21 19:04:39 -0500

    Add social redirect routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/premxai",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/premxai/",
+        permanent: true,
+      },
+      {
+        source: "/x",
+        destination: "https://x.com/Premxai",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://x.com/Premxai",
+        permanent: true,
+      },
+      {
+        source: "/medium",
+        destination: "https://medium.com/@prem.b.kanaparthi",
+        permanent: true,
+      },
+      {
+        source: "/scholar",
+        destination: "https://scholar.google.com/citations?user=yIWQSykAAAAAJ&hl=en",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
